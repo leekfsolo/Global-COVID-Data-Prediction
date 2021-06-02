925c761c16dbe77a932b662e0ae702d98ce89510
Ken <gialap2002@gmail.com>
2021-06-02 15:49:53 +0700

    China overview

--- bundle.js
+++ bundle.js
@@ -1,78 +1,101 @@
-const svg = d3.select('svg')
+    const svg = d3.select('svg')
 
-svg
-    .attr('width', 1200)
-    .attr('height', 700);
+    svg
+        .attr('width', 1200)
+        .attr('height', 700);
 
-const width = +svg.attr('width');
-const height = +svg.attr('height');
-const margin = { top: 50, right: 50, bottom: 80, left: 150 };
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
-const formatDate = (date) => {
-    let day = "",
-        month = "",
-        year = "",
-        dashes = 0;
-    for (let i = 0; i < date.length; i++) {
-        if (dashes === 0) {
-            if (date[i] === '/') dashes++;
-            else day += date[i];
+    const width = +svg.attr('width');
+    const height = +svg.attr('height');
+    const margin = { top: 100, right: 20, bottom: 100, left: 300 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    const formatDate = (date) => {
+        let day = "",
+            month = "",
+            year = "",
+            dashes = 0;
+        for (let i = 0; i < date.length; i++) {
+            if (dashes === 0) {
+                if (date[i] === '/') dashes++;
+                else day += date[i];
+            }
+            else if (dashes === 1) {
+                if (date[i] === '/') dashes++;
+                else month += date[i];
+            }
+            else year += date[i];
         }
-        else if (dashes === 1) {
-            if (date[i] === '/') dashes++;
-            else month += date[i];
-        }
-        else year += date[i];
+        const result = month + '/' + day + '/' + year;
+        return result;
     }
-    const result = month + '/' + day + '/' + year;
-    return result;
-}
 
-const render = data => {
-    const xValue = d => d.dateRep;
-    const xAxisLabel = "Days";
-    const yValue = d => d.cases;
-    const yAxisLabel = "Cases";
-    const radius = 5;
-    const xScale = d3.scaleTime()
-        .domain(d3.extent(data, xValue))
-        .range([0, innerWidth])
-        .nice();
-    const yScale = d3.scaleLinear()
-        .domain(d3.extent(data, yValue))
-        .range([innerHeight, 0])
-        .nice();
-    const g = svg.append('g')
-        .attr('transform', `translate(${margin.left},${margin.right})`);
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-    g.selectAll('circle').data(data)
-        .enter()
-        .append('circle')
-        .attr('cx', d => (xScale(xValue(d))))
-        .attr('cy', d => (yScale(yValue(d))))
-        .attr('r', radius)
-        .attr('fill', 'black')
-        .append('title')
-        .text(d => d.cases);
-    const xAxisG = g.append('g').call(xAxis)
-                    .attr('transform', `translate(0, ${innerHeight})`);
-    const yAxisG = g.append('g').call(yAxis);
+    const render = data => {
+        const xValue = d => d.dateRep;
+        const xAxisLabel = "DateRep";
+        const yValue = d => d.cases;
+        const yAxisLabel = "Cases";
+        const radius = 5;
+        const title = `${yAxisLabel} vs ${xAxisLabel} of China`;
+        const xScale = d3.scaleTime()
+            .domain(d3.extent(data, xValue))
+            .range([0, innerWidth])
+            .nice();
+        const yScale = d3.scaleLinear()
+            .domain(d3.extent(data, yValue))
+            .range([innerHeight, 0])
+            .nice();
+        const g = svg.append('g')
+            .attr('transform', `translate(${margin.left},${margin.bottom})`);
+        const xAxis = d3.axisBottom(xScale)
+                        .tickSize(-innerHeight)
+                        .tickPadding(10);
+        const yAxis = d3.axisLeft(yScale)
+                        .tickSize(-innerWidth)
+                        .tickPadding(10);
+        g.selectAll('circle').data(data)
+            .enter()
+            .append('circle')
+            .attr('cx', d => (xScale(xValue(d))))
+            .attr('cy', d => (yScale(yValue(d))))
+            .attr('r', radius)
+            .attr('fill', 'black')
+            .append('title')
+            .text(d => d.cases);
+        const xAxisG = g.append('g').call(xAxis)
+                        .attr('transform', `translate(0, ${innerHeight})`);
+        const yAxisG = g.append('g').call(yAxis);
+        xAxisG.append('text')
+                .attr('class', 'axisText')
+                .attr('x', innerWidth/2)
+                .attr('y', 50)
+                .attr('text-anchor', 'middle')
+                .text(xAxisLabel);
+        yAxisG.append('text')
+                .attr('class', 'axisText')
+                .attr('x', -innerHeight/2)
+                .attr('y', -50)
+                .attr('transform', 'rotate(-90)')
+                .attr('text-anchor', 'middle')
+                .text(yAxisLabel);
+        const titleLabel = g.append('text')
+                            .text(title)
+                            .attr('x', innerWidth/2)
+                            .attr('y', -20)
+                            .attr('text-anchor', 'middle')
+                            .attr('class', 'title-label');
+    }
 
-}
-
-d3.csv('../data7-12_2020/CN7-12_2020.csv').then(data => {
-    data.forEach(d => {
-        d.day = +d.day;
-        d.month = +d.month;
-        d.year = +d.year;
-        d.cases = +d.cases;
-        d.deaths = +d.deaths;
-        d['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']
-            = +d['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000'];
-        d.dateRep = new Date(formatDate(d.dateRep));
+    d3.csv('../data7-12_2020/CN7-12_2020.csv').then(data => {
+        data.forEach(d => {
+            d.day = +d.day;
+            d.month = +d.month;
+            d.year = +d.year;
+            d.cases = +d.cases;
+            d.deaths = +d.deaths;
+            d['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']
+                = +d['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000'];
+            d.dateRep = new Date(formatDate(d.dateRep));
+        })
+        console.log(data)
+        render(data);
     })
-    console.log(data)
-    render(data);
-})
