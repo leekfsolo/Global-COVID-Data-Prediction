b103b86faf1488b6c617dd88166384235fac3670
Ken <gialap2002@gmail.com>
2021-06-04 19:49:45 +0700

    basic multiple line plot

--- bundle.js
+++ bundle.js
@@ -5,25 +5,57 @@ const height = document.body.clientHeight;
 svg
     .attr('width', width)
     .attr('height', height);
-const marginLeftGraph = { top: 70, right: width/2+50, bottom: 100, left: 100 };
+const marginLeftGraph = {
+    top: 80,
+    right: 250,
+    bottom: 100,
+    left: 150
+};
 const innerWidth = width - marginLeftGraph.left - marginLeftGraph.right;
 const innerHeight = height - marginLeftGraph.top - marginLeftGraph.bottom;
 
+const colorLegend = (selection, props) => {
+    const {
+        textOffSet,
+        circleRadius,
+        colorScale,
+        spacing
+    } = props;
+
+    const groups = selection.selectAll('g')
+                    .data(colorScale.domain());
+    const groupsEnter = groups.enter()
+                            .append('g')
+                            .attr('class', 'tick');
+    groupsEnter.merge(groups)
+                .attr('transform', (d, i) => `translate(0, ${i*spacing})`)
+                groups.exit().remove();
+    groupsEnter.append('circle')
+                .attr('r', circleRadius)
+                .attr('fill', colorScale);
+    groupsEnter.append('text')
+                .attr('dy', '0.32em')
+                .attr('x', textOffSet)
+                .text(d => d);
+}
+
 const render = data => {
     const xValue = d => d.date;
-    const xAxisLabel = "Date";
+    const xAxisLabel = "Timeline";
+    const xTextOffset = 60;
     const yValue = d => d.new_cases;
     const yAxisLabel = "New cases";
-    const radius = 5;
-    const title = `${yAxisLabel} vs ${xAxisLabel} of China`;
+    const yTextOffset = 100;
+    const colorValue = d => d.location;
+    // const title = `${yAxisLabel} vs ${xAxisLabel} of China`;
     const xScale = d3.scaleTime()
         .domain(d3.extent(data, xValue))
-        .range([0, innerWidth])
-        .nice();
+        .range([0, innerWidth]);
     const yScale = d3.scaleLinear()
-        .domain(d3.extent(data, yValue))
+        .domain([0, d3.max(data, yValue)])
         .range([innerHeight, 0])
         .nice();
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
     const g = svg.append('g')
         .attr('transform', `translate(${marginLeftGraph.left},${marginLeftGraph.bottom})`);
     const xAxis = d3.axisBottom(xScale)
@@ -32,45 +64,49 @@ const render = data => {
     const yAxis = d3.axisLeft(yScale)
         .tickSize(-innerWidth)
         .tickPadding(10);
-    const lineGenerator = d3.line()
-        .x(d => xScale(xValue(d)))
-        .y(d => yScale(yValue(d)))
-        .curve(d3.curveBasis);
-    const nested = d3.nest()
-                        .key(d => d.location)
-                        .entries(data);
-    console.log(nested)
-    g.selectAll('.line-path').data(nested)
-        .enter()
-        .append('path')
-        .attr('class', 'line-path')
-        .attr('d', d => lineGenerator(d.values));
-
     const xAxisG = g.append('g').call(xAxis)
         .attr('transform', `translate(0, ${innerHeight})`);
     const yAxisG = g.append('g').call(yAxis);
     xAxisG.append('text')
         .attr('class', 'axisText')
         .attr('x', innerWidth / 2)
-        .attr('y', 50)
+        .attr('y', xTextOffset)
         .attr('text-anchor', 'middle')
         .text(xAxisLabel);
     yAxisG.append('text')
         .attr('class', 'axisText')
         .attr('x', -innerHeight / 2)
-        .attr('y', -50)
+        .attr('y', -yTextOffset)
         .attr('transform', 'rotate(-90)')
         .attr('text-anchor', 'middle')
         .text(yAxisLabel);
-    // const titleLabel = g.append('text')
-    //     .text(title)
-    //     .attr('x', innerWidth / 2)
-    //     .attr('y', -20)
-    //     .attr('text-anchor', 'middle')
-    //     .attr('class', 'title-label');
+
+    const lineGenerator = d3.line()
+        .x(d => xScale(xValue(d)))
+        .y(d => yScale(yValue(d)))
+        .curve(d3.curveBasis);
+    const lastYValue = d => yValue(d.values[d.values.length-1]);
+    const nested = d3.nest()
+        .key(colorValue)
+        .entries(data)
+        .sort((a,b) => d3.descending(lastYValue(a), lastYValue(b)));
+    g.selectAll('.line-path').data(nested)
+        .enter()
+        .append('path')
+        .attr('class', 'line-path')
+        .attr('d', d => lineGenerator(d.values))
+        .attr('stroke', d => colorScale(d.key));
+    colorScale.domain(nested.map(d => d.key));
+    svg.append('g').call(colorLegend, {
+        textOffSet: 40,
+        circleRadius: 15,
+        colorScale,
+        spacing: 80
+    })
+                .attr('transform', `translate(1280, 250)`)
 }
 
-d3.csv('./dataset/data-7-12-2020/data-7-12-2020.csv').then(data => {
+d3.csv('./continent_data.csv').then(data => {
     data.forEach(d => {
         d.new_cases = +d.new_cases;
         d.new_deaths = +d.new_deaths;
@@ -85,8 +121,3 @@ d3.csv('./dataset/data-7-12-2020/data-7-12-2020.csv').then(data => {
     render(data);
 })
 
-
-const x = document.body.clientWidth;
-const y = document.body.clientHeight;
-console.log(x);
-console.log(y);
