5f1756f33da5ec638374f8b4a025d5bc86e016b6
Ken <gialap2002@gmail.com>
2021-06-11 10:14:41 +0700

    data for prediction

--- bundle.js
+++ bundle.js
@@ -40,52 +40,6 @@ const colorLegend = (selection, props) => {
         .text(d => d);
 }
 
-const hover = (svg, path, data) => {
-    if ("ontouchstart" in document) svg
-        .style("-webkit-tap-highlight-color", "transparent")
-        .on("touchmove", moved)
-        .on("touchstart", entered)
-        .on("touchend", left)
-    else svg
-        .on("mousemove", moved)
-        .on("mouseenter", entered)
-        .on("mouseleave", left);
-
-    const dot = svg.append("g")
-        .attr("display", "none");
-
-    dot.append("circle")
-        .attr("r", 2.5);
-
-    dot.append("text")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10)
-        .attr("text-anchor", "middle")
-        .attr("y", -8);
-
-    function moved(event) {
-        event.preventDefault();
-        const pointer = d3.pointer(event, this);
-        // const xm = x.invert(pointer[0]);
-        // const ym = y.invert(pointer[1]);
-        // const i = d3.bisectCenter(data.date, xm);
-        // const s = d3.least(data.series, d => Math.abs(d.values[i] - ym));
-        // path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
-        // dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
-        // dot.select("text").text(s.name);
-    }
-
-    function entered() {
-        path.style("mix-blend-mode", null).attr("stroke", "#ddd");
-        dot.attr("display", null);
-    }
-
-    function left() {
-        path.style("mix-blend-mode", "multiply").attr("stroke", null);
-        dot.attr("display", "none");
-    }
-}
-
 const render = data => {
     const xValue = d => d.date;
     const xAxisLabel = "Timeline";
@@ -131,8 +85,8 @@ const render = data => {
         .x(d => xScale(xValue(d)))
         .y(d => yScale(yValue(d)))
         .curve(d3.curveBasis);
-    
-    const nested = d3.group(data, colorValue);
+    const lastYValue = d => yValue(d.values[d.values.length-1]);
+    const nested = d3.groups(data, colorValue);
     const path = g.selectAll('.line-path').data(nested)
         .enter()
         .append('path')
@@ -146,11 +100,55 @@ const render = data => {
         spacing: 80
     })
         .attr('transform', `translate(1280, 250)`)
-    
-    //svg.call(hover, path, data);
+    const hover = (svg, path) => {
+        if ("ontouchstart" in document) svg
+            .style("-webkit-tap-highlight-color", "transparent")
+            .on("touchmove", moved)
+            .on("touchstart", entered)
+            .on("touchend", left)
+        else svg
+            .on("mousemove", moved)
+            .on("mouseenter", entered)
+            .on("mouseleave", left);
+
+        const dot = svg.append("g")
+            .attr("display", "none");
+
+        dot.append("circle")
+            .attr("r", 2.5);
+
+        dot.append("text")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .attr("text-anchor", "middle")
+            .attr("y", -8);
+
+        function moved(event) {
+            event.preventDefault();
+            const pointer = d3.pointer(event, this);
+            const xm = xScale.invert(pointer[0]);
+            const ym = yScale.invert(pointer[1]);
+            const i = d3.bisectCenter(xValue, xm);
+            // const s = d3.least(data.series, d => Math.abs(d.values[i] - ym));
+            // path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
+            // dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
+            // dot.select("text").text(s.name);
+        }
+
+        function entered() {
+            path.style("mix-blend-mode", null).attr("stroke", "#ddd");
+            dot.attr("display", null);
+        }
+
+        function left() {
+            path.style("mix-blend-mode", "multiply").attr("stroke", null);
+            dot.attr("display", "none");
+        }
+    }
+    // svg.call(hover, path);
 }
 
-d3.csv('./continent_data2.csv').then(data => {
+d3.csv('./continent_data1.csv').then(data => {
     data.forEach(d => {
         d.new_cases = +d.new_cases;
         d.new_deaths = +d.new_deaths;
